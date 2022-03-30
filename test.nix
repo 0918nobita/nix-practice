c61bd7518921342bac740ede1cda546f65ac472a
0918nobita <nobita.0918@gmail.com>
2022-03-30 20:42:31 +0900

    :art: Fix indentation

--- test.nix
+++ test.nix
@@ -1,4 +1,3 @@
 { a, b ? 3, trueMsg ? "yes", falseMsg ? "no" }:
-    if a > b
-        then builtins.trace trueMsg true
-        else builtins.trace falseMsg false
+
+if a > b then builtins.trace trueMsg true else builtins.trace falseMsg false
